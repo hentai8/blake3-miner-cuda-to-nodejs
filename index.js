5e627aba70360a6d9c4456611b7cedd6186802e2
hentai8 <496983136@qq.com>
2022-10-11 16:19:49 +0800

    feature: init by node-js-cuda

---
+++ index.js
@@ -0,0 +1,24 @@
+/*
+This directory is system relative 
+*/
+
+var addon = require('/home/hentai8-desktop/Desktop/project/node-js-cuda/build/Release/addon.node');
+
+
+
+/*
+The first argument stand for: 
+     0 --> Addition
+     1 --> Subtract
+     2 --> Multiplication
+     3 --> Exponential
+     4 --> Power
+*/
+
+console.log(addon.math_func(0,4,5));
+console.log(addon.math_func(1,5,6));
+console.log(addon.math_func(2,5,6));
+//the 3rd argument is Ambiguous
+console.log(addon.math_func(3,2,5));
+console.log(addon.math_func(4,5,6));
+
